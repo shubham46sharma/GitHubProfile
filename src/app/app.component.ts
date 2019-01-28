0a7e1ea317edc6cc9f258b411a76351b3427f98d
shubham46sharma <shubham46sharma@gmail.com>
2019-01-28 11:59:33 +0530

    New commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:string = 'ProfileSearchPage';
+  rootPage:string = 'ProfileSearchPage' ;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
